cc18bb4dd6e560866337c614d45a44a27cbc9675
Keval Ambasana <kevalambasana@gmail.com>
2024-05-12 18:52:13 +0530

    Adds Homepage Component, Auth Module and Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'book-bliss';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.location = "Pune";   //Add function to select locations
+  }
+  constructor() {}
+  title = 'bookbliss';
+
+  location!: string;
+  homepageRenderFlag: boolean = true;
+
+  openLoginModal() {
+    console.log("Hi");
+    
+  }
+
+  openRegisterModal() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
